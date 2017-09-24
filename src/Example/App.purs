e7bb1618f39b224185935d417e51abc1976a1452
Felix Schlitter <felixschlitter@protonmail.com>
2017-09-25 07:58:28 +1300

    Add redux saga example

--- src/Example/App.purs
+++ src/Example/App.purs
@@ -1,126 +1,129 @@
 module Example.App (main) where
 
-import Prelude (Unit, (<$>), (+), (<>), (<<<), bind, id, pure, show, void)
+import Prelude
+import Redux.Saga
 
+import Control.Monad.Aff (delay)
+import Control.Monad.Aff.AVar (AVAR)
+import Control.Monad.Aff.Class (liftAff)
+import Control.Monad.Aff.Console as Console
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, info)
-import Control.Monad.Eff.Timer (TIMER, setTimeout)
+import Control.Monad.Eff.Console (CONSOLE)
+import Control.Monad.Eff.Exception (Error, EXCEPTION)
+import Control.Monad.Eff.Ref (REF)
 import Control.Monad.Eff.Unsafe (unsafeCoerceEff)
-
-import Data.Lens (Lens', Prism', lens, prism')
+import Data.Lens (Lens', Prism', lens, prism', to, view)
 import Data.Maybe (Maybe(..))
-
+import Data.Profunctor.Strong (first, second)
+import Data.Time.Duration (Milliseconds(..))
+import Data.Tuple (fst, snd, uncurry)
+import React (preventDefault)
 import React as React
 import React.DOM as DOM
+import React.DOM.Props (className)
 import React.DOM.Props as Props
-
+import React.Redux (REDUX)
 import React.Redux as Redux
 
-type State = { counterA :: Int, counterB :: Int }
+type GlobalState =
+  { isLoggingIn :: Boolean
+  , loginError :: Maybe Error
+  , loggedInAs :: Maybe { username :: String }
+  }
 
-type StateA = { counterA :: Int }
+data Action
+  = LoginRequest { username :: String, password :: String }
+  | LoginFailure Error
+  | LoginSuccess { username :: String }
 
-type StateB = { counterB :: Int }
+saga :: ∀ eff. Saga (console :: CONSOLE | eff) Action GlobalState Unit
+saga = do
+  void $ takeEvery case _ of
+    LoginRequest { username, password } -> pure do
+      liftAff $ Console.log $ "Logging in as " <> show username
+      liftAff $ delay $ 1000.0 # Milliseconds
+      pure unit
+    _ -> Nothing
 
-data Action = ActionA ActionA | ActionB ActionB
-
-data ActionA = IncrementA | DelayedIncrementA Int
-
-data ActionB = IncrementB
-
-type Effect eff = (console :: CONSOLE, timer :: TIMER | eff)
-
-store :: forall eff. Eff (Effect (Redux.ReduxEffect eff)) (Redux.Store Action State)
-store = Redux.createStore reducer initialState (middlewareEnhancer <<< reduxDevtoolsExtensionEnhancer')
+mkStore
+  :: ∀ eff
+   . Eff _ (Redux.Store Action GlobalState)
+mkStore = Redux.createStore reducer initialState (middlewareEnhancer <<< reduxDevtoolsExtensionEnhancer')
   where
-  initialState :: State
-  initialState = { counterA: 0, counterB: 0 }
+  initialState :: GlobalState
+  initialState = { isLoggingIn: false
+                 , loginError: Nothing
+                 , loggedInAs: Nothing
+                 }
 
-  reduxDevtoolsExtensionEnhancer' :: Redux.Enhancer (Effect eff) Action State
+  reduxDevtoolsExtensionEnhancer' :: Redux.Enhancer _ Action GlobalState
   reduxDevtoolsExtensionEnhancer' = Redux.fromEnhancerForeign reduxDevtoolsExtensionEnhancer
 
-  middlewareEnhancer :: Redux.Enhancer (Effect eff) Action State
-  middlewareEnhancer = Redux.applyMiddleware [ loggerMiddleware, timeoutSchedulerMiddleware ]
+  middlewareEnhancer :: Redux.Enhancer _ Action GlobalState
+  middlewareEnhancer = Redux.applyMiddleware [ sagaMiddleware saga ]
 
-  loggerMiddleware :: Redux.Middleware (Effect eff) Action State Unit
-  loggerMiddleware { getState, dispatch } next action = do
-    _ <- info showAction
-    _ <- next action
-    state <- getState
-    logState state
+  reducer :: Redux.Reducer Action GlobalState
+  reducer = flip go
     where
-    logState :: State -> Eff (Effect (Redux.ReduxEffect eff)) Unit
-    logState { counterA, counterB } = info ("state = { counterA: " <> show counterA <> ", " <> "counterB: " <> show counterB <> " }")
+    go state = case _ of
+      LoginRequest _ -> state { isLoggingIn = true }
+      LoginFailure _ -> state { isLoggingIn = false }
+      LoginSuccess _ -> state { isLoggingIn = false }
+      _ -> state
 
-    showAction :: String
-    showAction =
-      case action of
-           ActionA IncrementA -> "ActionA IncremementA"
-           ActionA (DelayedIncrementA delay) -> "ActionA (DelayedIncrementA " <> show delay <> ")"
-           ActionB IncrementB -> "ActionB IncremementB"
+type LocalState = { username :: String, password :: String }
 
-  timeoutSchedulerMiddleware :: Redux.Middleware (Effect eff) Action State Unit
-  timeoutSchedulerMiddleware { getState, dispatch } next action =
-    case action of
-         ActionA (DelayedIncrementA delay) -> void (setTimeout delay (void (next action)))
-         _ -> void (next action)
+appClass :: ∀ props. Redux.ReduxReactClass GlobalState props GlobalState
+appClass = Redux.createClass first spec
 
-  reducer :: Redux.Reducer Action State
-  reducer action =
-    (Redux.reducerOptic lensA prismA reducerA) action <<<
-    (Redux.reducerOptic lensB prismB reducerB) action
-    where
-    reducerA :: Redux.Reducer ActionA StateA
-    reducerA action' state' =
-      case action' of
-           IncrementA -> state' { counterA = state'.counterA + 1 }
-           DelayedIncrementA _ -> state' { counterA = state'.counterA + 1 }
-
-    lensA :: Lens' State StateA
-    lensA = lens (\s -> { counterA: s.counterA }) (\s b -> s { counterA = b.counterA })
-
-    prismA :: Prism' Action ActionA
-    prismA = prism' ActionA (\a -> case a of
-                                        ActionA a' -> Just a'
-                                        _ -> Nothing)
-
-    reducerB :: Redux.Reducer ActionB StateB
-    reducerB action' state' =
-      case action' of
-           IncrementB -> state' { counterB = state'.counterB + 1 }
-
-    lensB :: Lens' State StateB
-    lensB = lens (\s -> { counterB: s.counterB }) (\s b -> s { counterB = b.counterB })
-
-    prismB :: Prism' Action ActionB
-    prismB = prism' ActionB (\a -> case a of
-                                        ActionB a' -> Just a'
-                                        _ -> Nothing)
-
-appClass :: Redux.ReduxReactClass' State State
-appClass = Redux.createClass' id (Redux.spec' render)
   where
-  render :: forall eff. Redux.Render State Unit eff (Eff (Redux.ReduxEffect eff)) Action
-  render dispatch this = render' <$> React.getProps this
-    where
-    render' :: State -> React.ReactElement
-    render' props =
-      DOM.div []
-              [ DOM.button [ Props.onClick (onClick (ActionA IncrementA)) ]
-                           [ DOM.text ("Increment A: " <> show props.counterA) ]
-              , DOM.button [ Props.onClick (onClick (ActionA (DelayedIncrementA 2000))) ]
-                           [ DOM.text ("Increment A (delayed by 2s): " <> show props.counterA) ]
-              , DOM.button [ Props.onClick (onClick (ActionB IncrementB)) ]
-                           [ DOM.text ("Increment B: " <> show props.counterB) ]
-              ]
-      where
-      onClick :: Action -> React.Event -> React.EventHandlerContext eff Unit Unit Unit
-      onClick action event = void (unsafeCoerceEff (dispatch (pure action)))
+  spec :: Redux.Spec _ _ _ (Eff _) _
+  spec = Redux.spec (\_ _ -> pure { username: "", password: ""}) render
 
-main :: forall eff. Eff (Effect (Redux.ReduxEffect eff)) React.ReactElement
+  render dispatch this = render' <$> React.getProps this
+                                 <*> React.readState this
+    where
+    render' :: GlobalState -> LocalState -> React.ReactElement
+    render' { loggedInAs: Just { username } } _
+      = DOM.div []
+                [ DOM.text $ "Welcome back, " <> username ]
+    render' { isLoggingIn } state =
+      DOM.form
+        [ Props.className "box"
+        , Props.onSubmit $ \event -> do
+            void $ preventDefault event
+            { username, password } <- React.readState this
+            void $ dispatch $ pure $ LoginRequest { username, password }
+        ] $
+        [ DOM.h3 [ Props.className "title is-5" ]
+            [ DOM.text "Please login:" ]
+        , DOM.div [ Props.className "field" ] [
+            DOM.input [ Props.value state.username
+                      , Props.placeholder "username"
+                      , Props.className "input"
+                      , Props._type "text"
+                      ] []
+          ]
+        , DOM.div [ Props.className "field" ] [
+            DOM.input [ Props.value state.password
+                      , Props.placeholder "password"
+                      , Props.className "input"
+                      , Props._type "password"
+                      ] []
+          ]
+        , DOM.div [ Props.className "field" ] $ [
+            DOM.input [ Props.value "login"
+                      , Props._type "submit"
+                      , Props.className "button is-primary"
+                      , Props.disabled isLoggingIn
+                      ] []
+          ] <> if isLoggingIn
+                  then [ DOM.span [ Props.className "fa fa-spinner fa-spin" ] [] ]
+                  else []
+        ]
+main :: Eff _ React.ReactElement
 main = do
-  store' <- store
-  let element = Redux.createProviderElement store' appClass
-  pure element
+  store <- mkStore
+  pure $ Redux.createProviderElement store appClass
 
 foreign import reduxDevtoolsExtensionEnhancer :: forall action state. Redux.EnhancerForeign action state
